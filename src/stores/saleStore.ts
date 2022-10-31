1fa78428c33ea368e8615fbfa3dac639ed12ef99
Pablo Camino <pablo@cloudflare.com>
2022-10-31 10:06:29 -0500

    Handle auth and submit

--- src/stores/saleStore.ts
+++ src/stores/saleStore.ts
@@ -1,33 +1,30 @@
 import { writable } from 'svelte/store';
 import { supabaseClient } from '$lib/supabaseClient.js';
 export const gsales = writable();
-export const zip_cod= writable(0);
+export const zip_cod = writable(0);
 
-
-export async function loadGSales (){
-    const { data, error } = await supabaseClient.from('sales').select();
-    if (error) {
-        return console.error(error);
-    }
-    gsales.set(data);
+export async function loadGSales() {
+	const { data, error } = await supabaseClient.from('sales').select();
+	if (error) {
+		return console.error(error);
+	}
+	gsales.set(data);
 }
 
-export async function loadZipSales(zip: number){
-    const { data, error } = await supabaseClient.from('sales').select().eq('zip', zip )
-    if (error) {
-        return console.error(error);
-    }
-    gsales.set(data);
+export async function loadZipSales(zip: number) {
+	const { data, error } = await supabaseClient.from('sales').select().eq('zip', zip);
+	if (error) {
+		return console.error(error);
+	}
+	gsales.set(data);
 }
-
-
 
 export async function addGSales(gsale: any) {
-    const { data, error } = await supabaseClient.from('recipes').insert(gsale);
+	const { data, error } = await supabaseClient.from('recipes').insert(gsale);
 
-    if (error) {
-        return console.error(error);
-    }
+	if (error) {
+		return console.error(error);
+	}
 
-    gsales.update((cur: any) => [...cur, data[0]]);
+	gsales.update((cur: any) => [...cur, data[0]]);
 }
