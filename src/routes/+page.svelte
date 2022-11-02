7a446891b678ea23d92a3d113abf70ac7a6119de
Pablo Camino <pablo@cloudflare.com>
2022-11-02 10:22:24 -0500

    Change the file strucuture

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <script>
-	import Map from '../components/Map.svelte';
-	import Blog from '../components/Blog.svelte';
-	import ButtonSale from '../components/ButtonSale.svelte';
-	import Table from '../components/Table.svelte';
-	import { gsales, loadGSales } from '../stores/saleStore';
+	import Map from '$lib/components/Map.svelte';
+	import Blog from '$lib/components/Blog.svelte';
+	import ButtonSale from '$lib/components/ButtonSale.svelte';
+	import Table from '$lib/components/Table.svelte';
+	import { gsales, loadGSales } from '../lib/stores/saleStore';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	loadGSales();
 
 </script>
+<Navbar />
 
 <ButtonSale />
 {#await $gsales then dataSales}
